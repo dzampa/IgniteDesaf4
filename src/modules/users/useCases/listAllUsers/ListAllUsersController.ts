53100a8bf8ca38fc5590e29a50c2b412ae47428f
Diego Zamparo <dzamparo@gmail.com>
2021-03-23 19:03:58 -0300

    first commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = <{ user_id: string }>request.headers;
+      const all = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export { ListAllUsersController };
